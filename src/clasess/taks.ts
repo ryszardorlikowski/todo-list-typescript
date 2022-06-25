f273140e4938baf4ae82c88ebdd39881821d56e5
Ryszard Orlikowski <ryszardorlikowski93@gmail.com>
2022-06-25 12:34:58 +0200

    Add task class

--- src/clasess/taks.ts
+++ src/clasess/taks.ts
@@ -1,18 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export class Task {
-    id: number;
-    name: string;
-    description: string;
-    completed: boolean;
-    createdAt: Date
+  id: string;
+  name: string;
+  description: string;
+  completed: boolean;
+  createdAt: Date;
 
-    constructor(name: string, description: string) {
-        this.id = uuidv4();
-        this.name = name;
-        this.description = description;
-        this.completed = false;
-        this.createdAt = new Date();
-    }
-
+  constructor(name: string, description: string) {
+    this.id = uuidv4();
+    this.name = name;
+    this.description = description;
+    this.completed = false;
+    this.createdAt = new Date();
+  }
 }
